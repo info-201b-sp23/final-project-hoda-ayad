6c772fbbfe51aa397bad5997a36f5f89e2fef420
yyosup <yosoup@uw.edu>
2023-05-30 14:11:26 -0700

    Added saves into summary

--- ui.R
+++ ui.R
@@ -10,14 +10,31 @@ recent_titles_df <- titles_df %>%
 
 intro_panel <- tabPanel (
   "Introduction",
-  titlePanel("[INSERT TITLE]"),
+  titlePanel("Introduction Page"),
   br(),
   
-  p("For this final project, our group intends to determine whether the actors shown in Netflix movies/shows serve as a reliable indicator of the show/movie’s ratings. We want to see whether there is a direct relationship between the actors used in the shows/movies, and the ratings that the movies/shows receive after its initial release. It is important that we answer this question because of the value our project results can hold for streaming companies and film/show development firms in general, as there have been instances where the actors used did not make up for the low ratings the film/show received. Although it is important that good actors are used to make a good film, we want to see whether using famous actors boost the ratings the show/movie receives compared to using mediocre or lesser-known ones. In order to determine this, our group seeks to answer the following (but not limited to) possible research questions:
+  p(strong("Abstract")),
+  
 
-What role have actors played in the overall success of a movie? What is the relationship between the actors used in Netflix movies/shows and the third-party ratings the movies/show receives (e.g. Rotten Tomato, IMDb)? Has the choice in actors portrayed in Netflix shows/movies impacted the ratings its viewers give it?")
+  P("Having big names in the cast has always been a key marketing tactic for movies and TV shows alike, but does this strategy actually work? We intend to use the metadata of the shows and movies published on Netflix as of July 2022 to determine whether the major actor(s) credited in a piece of media is a significant indicator of a higher IMDb score as a representation of general reception. Considering there are many different contributing factors to IMDb scores, we will also take into account the number of votes on IMDb, the release year, and the maturity rating of the work to compare the impact on reception."),
+  
+  br(),
+  
+  p(strong("Introduction")),
+  
+  p("For this final project, our group intends to determine whether the actors shown in Netflix movies/shows serve as a reliable indicator of the show/movie’s ratings. We want to see whether there is a direct relationship between the actors used in the shows/movies, and the ratings that the movies/shows receive after its initial release. It is important that we answer this question because of the value our project results can hold for streaming companies and film/show development firms in general, as there have been instances where the actors used did not make up for the low ratings the film/show received. Although it is important that good actors are used to make a good film, we want to see whether using famous actors boost the ratings the show/movie receives compared to using mediocre or lesser-known ones."),
+  
+  br(),
+  
+  p("In order to determine this, our group seeks to answer the following (but not limited to) possible research questions:"),
+  
+  p(strong("What role have actors played in the overall success of a movie?")),
+  
+  p(strong("What is the relationship between the actors used in Netflix movies/shows and the third-party ratings the movies/show receives (e.g. Rotten Tomato, IMDb)?")),
+  
+  p(strong("Has the choice in actors portrayed in Netflix shows/movies impacted the ratings its viewers give it?"))
+  
 )
-
 
 # Genre Chart Sidebar
 genre_sidebar <- sidebarPanel(
@@ -75,10 +92,17 @@ Movies_VS_Shows <- tabPanel(
   )
 )
 
+concl_panel <- tabPanel(
+  "Conlcusion",
+  titlePanel("Conclusion and Summary Takeaways"),
+  br(), 
+  
+)
 ui <- navbarPage(
   "Movie Stars to 5 Stars: Actor Credits as Indicators of IMDb Score",
   intro_panel,
   genre_panel,
-  Movies_VS_Shows
+  Movies_VS_Shows,
+  concl_panel
 )
 
